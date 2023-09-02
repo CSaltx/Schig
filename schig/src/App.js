54c9681b82ae7aa528437bcdb58033171adea7e3
CSaltx <cobytyler7@gmail.com>
2023-09-01 18:26:02 -0700

    added routing

--- schig/src/App.js
+++ schig/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./Components/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <h1>SCHIG</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 export default App;
